9202faa5ee3f1c263d1d4dd1700c4b4effbb82bb
kevin <kevinskw@outlook.com>
2024-05-05 14:11:18 -0400

    LASSO solved

--- functions/LASSO.py
+++ functions/LASSO.py
@@ -1,5 +1,8 @@
 import cvxpy as cp
 import numpy as np
+from scipy.stats import gmean
+
+from functions.helper import cal_Q
 
 
 def LASSO(returns, factRet, lambda_, K):
@@ -19,5 +22,13 @@ def LASSO(returns, factRet, lambda_, K):
     # mu =          % n x 1 vector of asset exp. returns
     # Q  =          % n x n asset covariance matrix
     # ----------------------------------------------------------------------
-
+    factRet = np.hstack([np.ones((len(factRet), 1)), factRet])
+    beta = cp.Variable((factRet.shape[1], returns.shape[1]))
+    residual = returns.values - factRet @ beta
+    loss = cp.sum_squares(residual) + lambda_ * cp.norm(beta, 1)
+    problem = cp.Problem(cp.Minimize(loss))
+    problem.solve()
+    factor_mu = gmean(factRet + 1) - 1
+    mu = np.dot(factor_mu, beta.value)
+    Q = cal_Q(beta.value, factRet, returns - factRet @ beta.value)
     return mu, Q
