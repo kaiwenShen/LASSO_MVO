d404fdbbc6f04eeef6df205b5026b21ec3ae1e1a
kevin <kevinskw@outlook.com>
2024-05-05 09:47:44 -0400

    modified to allow adjusted_r2 output

--- functions/OLS.py
+++ functions/OLS.py
@@ -1,6 +1,6 @@
 # import cvxpy as cp
 import numpy as np
-from functions.helper import r_squared, adjusted_r_squared
+from functions.helper import adjusted_r_squared
 from scipy.stats import gmean
 
 
@@ -30,11 +30,9 @@ def OLS(returns, factRet, lambda_, K):
         beta = np.dot(np.linalg.pinv(xtx), xty)
     factor_mu = gmean(factRet + 1) - 1
     mu = np.dot(factor_mu, beta)
-    print(f'OLS insample R2: \n{r_squared(returns, np.dot(factRet, beta))}')
-    print(f'OLS insample adj R2: \n{adjusted_r_squared(returns, np.dot(factRet, beta), factRet.shape)}')
     # compute the vcov matrix with formula Q = B'FB + delta
     residuals = returns - np.dot(factRet, beta)
     delta = np.diag(np.var(residuals, axis=0))
     F = np.cov(factRet, rowvar=False)
     Q = np.dot(np.dot(beta.T, F), beta) + delta
-    return mu, Q
+    return mu, Q, adjusted_r_squared(returns, np.dot(factRet, beta), factRet.shape)
